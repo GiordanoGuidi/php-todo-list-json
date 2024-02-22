7fbefbc46df818d749ea37012d7a09c6288e9882
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-22 18:57:50 +0100

    added call api in script.js

--- my_app/script.js
+++ my_app/script.js
@@ -1,5 +1,16 @@
 const { createApp } = Vue;
+console.log('Vue ok', Vue);
+const endpoint = 'http://localhost:8888/boolean/php-todo-list-json/api/script.php';
 const app = createApp({
+    data: () => ({
+        tasks: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+            console.log(this.tasks)
+        })
+    }
 
 });
-app.mount('root');
+app.mount('#app');
