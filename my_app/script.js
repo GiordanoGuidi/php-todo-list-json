e33b6a24b436dc3a15ab04e0b875764d85b4e62a
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-23 15:08:41 +0100

    added method addTask

--- my_app/script.js
+++ my_app/script.js
@@ -3,20 +3,22 @@ console.log('Vue ok', Vue);
 const endpoint = 'http://localhost:8888/boolean/php-todo-list-json/api/script.php';
 const app = createApp({
     data: () => ({
-        tasks: []
+        tasks: [],
+        newTask: "",
     }),
     methods: {
         addTask() {
             const data = { task: this.newTask }
             const config = { headers: { 'Content-type': 'multipart/form-data' } }
-            axios.post(endpoint, data, config)
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+                this.newTask = '';
+            })
         }
-
     },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data;
-            console.log(this.tasks)
         })
     }
 
